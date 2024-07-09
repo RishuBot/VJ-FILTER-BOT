d25e2b01d31478e5bd39bc70367d9ae58deaa9b0
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-09 18:53:54 +0530

    Update and rename TechVJ to commands.py

---
+++ CloneTechVJ/commands.py
@@ -0,0 +1,15 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+import os, string, logging, random, asyncio, time, datetime, re, sys, json, base64
+from Script import script
+from pyrogram import Client, filters, enums
+from pyrogram.errors import ChatAdminRequired, FloodWait
+from pyrogram.types import *
+from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files
+from database.users_chats_db import db
+from info import *
+from utils import get_settings, pub_is_subscribed, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial, get_seconds
+logger = logging.getLogger(__name__)
+
