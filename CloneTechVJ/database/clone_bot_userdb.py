9680752db48b752777bb06ce7304952dd16d0fde
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-10 07:18:09 +0530

    Update clone_bot_userdb.py

--- CloneTechVJ/database/clone_bot_userdb.py
+++ CloneTechVJ/database/clone_bot_userdb.py
@@ -6,7 +6,18 @@ class Database:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.user = self.db.user
+
+    async def add_user(self, bot_id, user_id):
+        user = {'user_id': user_id}
+        await self.db[str(bot_id)].insert_one(user)
+    
+    async def is_user_exist(self, bot_id, id):
+        user = await self.db[str(bot_id)].find_one({'user_id': int(id)})
+        return bool(user)
+    
+    async def total_users_count(self, bot_id):
+        count = await self.db[str(bot_id)].count_documents({})
+        return count
 
 
-db = Database(CLONE_DATABASE_URI, DATABASE_NAME)
+clonedb = Database(CLONE_DATABASE_URI, DATABASE_NAME)
