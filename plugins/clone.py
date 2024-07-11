10edc0e1d6ccfed26529390b6e66c8e96bda9cea
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-11 17:03:37 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -2,7 +2,7 @@ from info import API_ID, API_HASH, LOG_CHANNEL
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, BotCommand
 from utils import temp
-from database.users_chat_db import db
+from database.users_chats_db import db
 import re
 import pytz
 from datetime import datetime
@@ -12,3 +12,35 @@ import time
 
 @Client.on_message(filters.command('clone'))
 async def clone_menu(client, message):
+    techvj = await client.ask(message.chat.id, '<b>1) sᴇɴᴅ <code>/newbot</code> ᴛᴏ @BotFather\n2) ɢɪᴠᴇ ᴀ ɴᴀᴍᴇ ꜰᴏʀ ʏᴏᴜʀ ʙᴏᴛ.\n3) ɢɪᴠᴇ ᴀ ᴜɴɪǫᴜᴇ ᴜsᴇʀɴᴀᴍᴇ.\n4) ᴛʜᴇɴ ʏᴏᴜ ᴡɪʟʟ ɢᴇᴛ ᴀ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ʏᴏᴜʀ ʙᴏᴛ ᴛᴏᴋᴇɴ.\n5) ꜰᴏʀᴡᴀʀᴅ ᴛʜᴀᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴇ.\n\n/cancel - ᴄᴀɴᴄᴇʟ ᴛʜɪs ᴘʀᴏᴄᴇss.</b>')
+    if techvj.text == '/cancel':
+        await techvj.delete()
+        return await message.reply('<b>ᴄᴀɴᴄᴇʟᴇᴅ ᴛʜɪs ᴘʀᴏᴄᴇss 🚫</b>')
+    if techvj.forward_from and techvj.forward_from.id == 93372553:
+        try:
+            bot_token = re.findall(r"\b(\d+:[A-Za-z0-9_-]+)\b", techvj.text)[0]
+        except:
+            await techvj.delete()
+            return await message.reply('<b>sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ 😕</b>')
+    else:
+        await techvj.delete()
+        return await message.reply('<b>ɴᴏᴛ ꜰᴏʀᴡᴀʀᴅᴇᴅ ꜰʀᴏᴍ @BotFather 😑</b>')
+    await techvj.delete()
+    try:
+        user_id = message.from_user.id
+        msg = await message.reply_text("**👨‍💻 ᴡᴀɪᴛ ᴀ ᴍɪɴᴜᴛᴇ ɪ ᴀᴍ ᴄʀᴇᴀᴛɪɴɢ ʏᴏᴜʀ ʙᴏᴛ ❣️**")
+        try:
+            vj = Client(
+                f"{bot_token}", API_ID, API_HASH,
+                bot_token=bot_token,
+                plugins={"root": "CloneTechVJ"},
+            )
+            await vj.start()
+            bot = await vj.get_me()
+            await db.add_clone_bot(bot.id, user_id)
+            await msg.edit_text(f"<b>sᴜᴄᴄᴇssғᴜʟʟʏ ᴄʟᴏɴᴇᴅ ʏᴏᴜʀ ʙᴏᴛ: @{bot.username}.\n\n</b>")
+        except BaseException as e:
+            logging.exception("Error while cloning bot.")
+            await msg.edit_text(f"⚠️ <b>Bot Error:</b>\n\n<code>{e}</code>\n\n**Kindly forward this message to @KingVJ01 to get assistance.**")
+    except Exception as e:
+        logging.exception("Error while handling message.")
