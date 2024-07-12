febf27fad838638000236bff3e98d72875feb594
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-12 07:15:50 +0530

    Update commands.py

--- CloneTechVJ/commands.py
+++ CloneTechVJ/commands.py
@@ -102,7 +102,7 @@ async def start(client, message):
             size=get_size(files1.file_size)
             f_caption=files1.caption
             if f_caption is None:
-                f_caption = f"{' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files1.file_name.split()))}"
+                f_caption = f"@VJ_Botz  {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files1.file_name.split()))}"
             if cd["update_channel_link"] != None:
                 up = cd["update_channel_link"]
                 button = [[
