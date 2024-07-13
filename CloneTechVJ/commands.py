58fda27c1f3f09641f8e0aea1f1db8f77aa73d5d
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-13 20:21:14 +0530

    Update commands.py

--- CloneTechVJ/commands.py
+++ CloneTechVJ/commands.py
@@ -122,7 +122,7 @@ async def start(client, message):
                 chat_id=message.from_user.id,
                 file_id=file_id,
                 caption=f_caption,
-                protect_content=True if pre == 'filep' else False,
+                protect_content=False,
                 reply_markup=reply_markup
             )
             filesarr.append(msg)
