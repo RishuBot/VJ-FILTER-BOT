4d09f2a473c4129a8a03e0095f05c54f85e3d0d6
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-13 14:57:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -75,9 +75,10 @@ async def start():
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
     await TechVJBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
-    print("Restarting All Clone Bots.......")
-    await restart_bots()
-    print("Restarted All Clone Bots.")
+    if CLONE_MODE == True:
+        print("Restarting All Clone Bots.......")
+        await restart_bots()
+        print("Restarted All Clone Bots.")
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
