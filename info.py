8deb6393303c2b40b57d8b5dc3167dbe72aa307b
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-10-03 09:25:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -133,7 +133,7 @@ YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998",
 
 
 # Online Stream and Download
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
+STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
